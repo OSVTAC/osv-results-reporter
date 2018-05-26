e53458ef7caa14369dafb7c06ee95c2fe4e78db6
Chris Jerdonek <chris.jerdonek@gmail.com>
2018-05-26 03:43:58 -0700

    Stub out a function that creates a PDF.

--- src/orr/writers/pdfwriting.py
+++ src/orr/writers/pdfwriting.py
@@ -1,1 +1,18 @@
-# TODO
+import os
+
+from reportlab.pdfgen.canvas import Canvas
+
+
+# TODO: keep working on PDF generation.  This is a scratch function.
+def make_pdf(path, text):
+    """
+    Args:
+      path: a path-like object.
+      text: the text to include, as a string.
+    """
+    # Convert the path to a string for reportlab.
+    path = os.fspath(path)
+    canvas = Canvas(path)
+    canvas.drawString(200, 500, text)
+    canvas.showPage()
+    canvas.save()
